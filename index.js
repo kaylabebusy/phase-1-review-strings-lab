07aa24c91a3b11c602fcbc7f46b5204efa1d3394
Kayla Copeland <kaylabebusy@gmail.com>
2023-04-02 20:22:45 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Loki';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
